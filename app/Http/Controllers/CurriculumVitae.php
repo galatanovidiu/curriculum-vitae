eb4e8c8a12184368f416043c238ef3f8611eaae8
Ovidiu Galatan <galatanovidiu@gmail.com>
2021-11-03 10:11:18 +0200

    test github actions - 1

--- app/Http/Controllers/CurriculumVitae.php
+++ app/Http/Controllers/CurriculumVitae.php
@@ -7,7 +7,7 @@ class CurriculumVitae extends Controller
     public function index()
     {
         return inertia('CurriculumVitae', [
-
+            'no_data' => 'Just for testing'
         ]);
     }
 }
